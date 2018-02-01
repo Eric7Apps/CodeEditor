fcbd4bb1ab4cef50a70ae64356a81278f388b6c8
Eric Chauvin <eric4apps@gmail.com>
2018-02-01 11:08:03 -0700

    Update MSBuilder.cs

--- MSBuilder.cs
+++ MSBuilder.cs
@@ -7,7 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using System.Diagnostics; // For starting a program/process.
+using System.Diagnostics;
 using System.IO;
 
 
@@ -55,17 +55,10 @@ namespace CodeEditor
     MSBuildProcess.StartInfo.RedirectStandardOutput = false;
     MSBuildProcess.StartInfo.RedirectStandardInput = false;
 
-    // Use the full path so you don't have to set
-    // the Environment variable.
-    // So it can find the MSBuild.exe:
-    // Set Envirnment Path to
-    // c:\Windows\Microsoft.NET\Framework\v4.0.30319
-    // MSBuild.exe MyProj.csproj
-
+    MForm.ShowStatus( "Working Directory: " + WorkingDirectory );
     MForm.ShowStatus( "Starting: " + ProjectFileName );
 
     MSBuildProcess.StartInfo.FileName = ProjectFileName;
-    // MSBuildProcess.StartInfo.FileName = "c:\\Eric\\ClimateModel\\BuildProj.bat";
     MSBuildProcess.StartInfo.Verb = ""; // "Print";
     MSBuildProcess.StartInfo.CreateNoWindow = true;
     MSBuildProcess.StartInfo.ErrorDialog = false;
@@ -78,7 +71,7 @@ namespace CodeEditor
     }
     catch( Exception Except)
       {
-      MForm.ShowStatus( "Could not start the MSBuild program." );
+      MForm.ShowStatus( "Could not start the Build batch file." );
       MForm.ShowStatus( Except.Message );
       if( MSBuildProcess != null )
         {
@@ -92,65 +85,6 @@ namespace CodeEditor
 
 
 
-  /*
-  internal bool IsMSBuildFinished()
-    {
-    try
-    {
-    if( MSBuildProcess == null )
-      return true;
-
-    // MSBuildProcess.Refresh();
-    // "After Refresh is called, the first request
-    // for information about each property causes the
-    // process component to obtain a new value from
-    // the associated process."
-
-    // "The Process component is a snapshot of the
-    // process resource at the time they are
-    // associated. To view the current values for
-    // the associated process, call the Refresh method."
-
-    // Console.WriteLine("Physical Memory Usage: "
-       //   + myProcess.WorkingSet.ToString());
-
-    // MForm.ShowStatus( "Before HasExited()." );
-
-    if( !MSBuildProcess.HasExited )
-      {
-      // Don't do this here.  ShowMSBuildLines();
-      MForm.ShowStatus( " " );
-      MForm.ShowStatus( "MSBuild seconds: " + BuildStartTime.GetSecondsToNow().ToString("N2") );
-      // MForm.ShowStatus( " " );
-      return false;
-      }
-
-    // MForm.ShowStatus( "After HasExited()." );
-
-    // BuildTimer.Stop();
-
-    MForm.ShowStatus( "MSBuild has exited." );
-    MForm.ShowStatus( " " );
-
-    ShowMSBuildLines();
-
-    MSBuildProcess.Dispose();
-    MSBuildProcess = null;
-
-    MForm.ShowStatus( " " );
-    MForm.ShowStatus( "Finished with MSBuild." );
-    return true;
-
-    }
-    catch( Exception Except )
-      {
-      MForm.ShowStatus( "Exception in MSBuilder.IsMSBuildFinished(). " + Except.Message );
-      return true; // True to finish it.
-      }
-    }
-    */
-
-
   internal void DisposeOfEverything()
     {
     if( MSBuildProcess == null )
@@ -162,32 +96,43 @@ namespace CodeEditor
 
 
 
-  /*
-  internal void ShowMSBuildLines()
-    {
-    if( MSBuildProcess == null )
-      return;
+//////////////////////////////////////////
+// Example batch file to call MSBuild.exe:
+/*
 
-    // "Synchronous read operations introduce a
-    // dependency between the caller reading from the
-    // StandardOutput stream and the child process
-    // writing to that stream. These dependencies can
-    // result in deadlock conditions."
+@echo off
 
-    // Refresh();
-    StreamReader SReader = MSBuildProcess.StandardOutput;
-    while( SReader.Peek() >= 0 )
-      {
-      string Line = SReader.ReadLine();
-      // string Line = SReader.ReadToEnd();
-      if( Line == null )
-        continue;
+rem echo Test this.
 
-      MForm.ShowStatus( Line );
-      }
-    }
-    */
+rem Command line options:
+rem https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-command-line-reference
 
+rem Common Properties:
+rem https://docs.microsoft.com/en-us/visualstudio/msbuild/common-msbuild-project-properties
+
+rem MSBuild Reference:
+rem https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-reference
+
+rem MSBuild in Wikipedia:
+rem https://en.wikipedia.org/wiki/MSBuild
+
+rem MSBuild Task Reference:
+rem https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-task-reference
+
+rem Visual C++ Tasks:
+rem https://msdn.microsoft.com/en-us/library/ff960151.aspx
+
+rem This should already be running in the
+rem working directory this batch file is in.
+rem cd c:\Eric\ClimateModel
+
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe  /nologo /p:Configuration=Release /p:DebugSymbols=false /p:DefineDebug=false /p:DefineTrace=false /fileLogger /verbosity:normal ComputationLinks.csproj
+
+rem Help:
+rem c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe  /help
+
+pause
+*/
 
   }
 }
